a7bad81ffa04781fea38f8db91f2b579471b2878
Joas Bouwknegt <j.bouwknegt.3@student.rug.nl>
2023-11-13 12:05:38 +0100

    Project initialized with renv

--- scripts/first_script.R
+++ scripts/first_script.R
@@ -1,1 +1,14 @@
 # First github synced script
+
+# Load libraries
+library(renv)
+
+# Restore the package versions of the packages used to develop this project
+renv::restore()
+
+# Make a test graph to check if everything works
+x <- c(1,2,3,4,5)
+y <- c(1,4,3,6,9)
+
+data <- data.frame(x,y)
+
