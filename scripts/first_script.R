fb708f0354596946eee59fc5633c452a7721117c
Joas Bouwknegt <j.bouwknegt.3@student.rug.nl>
2023-11-13 12:23:39 +0100

    Fig01 improved

--- scripts/first_script.R
+++ scripts/first_script.R
@@ -2,6 +2,7 @@
 
 # Load libraries
 library(renv)
+library(tidyverse)
 
 # Restore the package versions of the packages used to develop this project
 renv::restore()
@@ -11,4 +12,9 @@ x <- c(1,2,3,4,5)
 y <- c(1,4,3,6,9)
 
 data <- data.frame(x,y)
+fig01 <- data |> ggplot(aes(x=x,y=y)) +
+  geom_point()+
+  geom_smooth(method="lm")
+fig01
+ggsave("./figures/fig01.png",plot=fig01,width=1920,height=1068,units="px")
 
